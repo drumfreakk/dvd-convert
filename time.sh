5a6677af6fae9c2585b37757adffb8061705c225
Kieran H <kieran.houtgraaf@gmail.com>
2025-02-26 19:29:49 +0100

    hopefully finished time script

--- time.sh
+++ time.sh
@@ -1,29 +1,70 @@
 #!/usr/bin/zsh
 
-echo "Total available time:                      " $(( 120 * 60 )) "s,\t 120\tmin\n"
+#Arguments: current position in line, desired position in line
+function align_upto () {
+	local cur_pos=$1
+	while [[ $cur_pos -lt $2 ]]; do
+		printf " "
+		cur_pos=$((cur_pos + 1))
+	done
+}
 
-cd ../converted/
+function echo_entry () {
 
-total_t=0
+	printf "$1"
+	
+	local cur_pos=${#1}
 
-for file in *; do
-    if [ -f "$file" ]; then
-		strlen=${#file}
-		cur_pos=$strlen
+	if [[ $cur_pos -ge 43 ]]; then
+		printf "\n"
+		cur_pos=0
+	fi
+	
+	align_upto $cur_pos 43
 
-		len_s=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${file}`
-		
-		total_t=$(( $total_t + $len_s ))
+	printf "${2%.*}  \ts,\t%.1f\tmin\n" $(( $2 / 60 ))
+}
 
-		echo -n $file
+function video_len () {
+	len=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1`
 
-		while [ ${cur_pos} -lt 44 ]; do
-			echo -n " "
-			cur_pos=$(( $cur_pos + 1 ))
-		done
+	if [[ "$len" != "N/A" ]]; then
+		printf $len
+	fi
+}
 
-		echo ${len_s%.*} "s,\t" $(printf "%.1f" $(( $len_s / 60 )) ) "\tmin"
-    fi
-done
+# Arguments: path from current dir, display path
+function search_dir () {
+	echo "\n" $2
 
-echo "\nTotal time:                                " ${total_t%.*} "s,\t" $(printf "%.1f" $(( $total_t / 60 )) ) "\tmin"
+	cd $1
+
+	local dir_t=0
+
+	local directories=()
+
+	for file in *; do
+		if [ -f "$file" ]; then
+			len_s=$(video_len $file)
+			dir_t=$(( dir_t + len_s ))
+	
+			echo_entry $file $len_s
+	    elif [[ -d "$file" ]]; then
+			directories+=($file)
+		fi
+	done
+
+	for dir in $directories; do
+		search_dir $dir "$2/$dir"
+		dir_t=$(( dir_t + ? ))
+	done
+
+	echo_entry "$2 total:" $dir_t
+	echo ""
+	cd ..
+
+	return dir_t
+}
+
+echo_entry "Total available time:" $(( 120 * 60 ))
+search_dir $1 $1
