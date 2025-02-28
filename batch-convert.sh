acc764364e5201eed47c2112a780eb85e4a976a7
Kieran H <kieran.houtgraaf@gmail.com>
2025-02-28 21:38:05 +0100

    nice

--- batch-convert.sh
+++ batch-convert.sh
@@ -8,6 +8,6 @@ cd ../
 for file in *; do
     if [ -f "$file" ]; then
 		echo "\n\n\nConverting ${file}"
-		ffmpeg -i $file -target pal-dvd -vf 'scale=1024:576:force_original_aspect_ratio=decrease,pad=1024:576:(ow-iw)/2:(oh-ih)/2,setsar=1' converted/${file%.*}.mpg
+		nice -n 19 ffmpeg -i $file -target pal-dvd -vf 'scale=1024:576:force_original_aspect_ratio=decrease,pad=1024:576:(ow-iw)/2:(oh-ih)/2,setsar=1' converted/${file%.*}.mpg
     fi
 done
