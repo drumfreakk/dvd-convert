9eedcebd4cba7f4b72a7af56427257a5d7afb4f1
Kieran H <kieran.houtgraaf@gmail.com>
2025-02-28 21:32:52 +0100

    time finished ???

--- time.sh
+++ time.sh
@@ -15,22 +15,30 @@ function echo_entry () {
 	
 	local cur_pos=${#1}
 
-	if [[ $cur_pos -ge 43 ]]; then
+	if [[ $cur_pos -ge 55 ]]; then
 		printf "\n"
 		cur_pos=0
 	fi
 	
-	align_upto $cur_pos 43
+	align_upto $cur_pos 55
 
 	printf "${2%.*}  \ts,\t%.1f\tmin\n" $(( $2 / 60 ))
 }
 
 function video_len () {
-	len=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1`
+	video_exts=("mp4" "mpg" "webm" "flv" "m4v" "mkv" "mov")
 
-	if [[ "$len" != "N/A" ]]; then
-		printf $len
+	ext=${1[-3,-1]}
+
+	if (( $video_exts[(Ie)$ext] )); then
+		len=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1`
+	
+		if [[ $? -eq 0 && "$len" != "N/A" ]]; then
+			printf $len
+			return 0
+		fi
 	fi
+	return -1
 }
 
 # Arguments: path from current dir, display path
@@ -43,21 +51,27 @@ function search_dir () {
 
 	local directories=()
 
-	for file in *; do
-		if [ -f "$file" ]; then
-			len_s=$(video_len $file)
-			dir_t=$(( dir_t + len_s ))
+	if [[ $(ls | wc -l) -ne 0 ]]; then
+		for file in *; do
+			if [ -f "$file" ]; then
+				len_s=$(video_len $file)
+				if [[ $? -eq 0 ]]; then
+					dir_t=$(( dir_t + len_s ))
 	
-			echo_entry $file $len_s
-	    elif [[ -d "$file" ]]; then
-			directories+=($file)
-		fi
-	done
-
-	for dir in $directories; do
-		search_dir $dir "$2/$dir"
-		dir_t=$(( dir_t + ? ))
-	done
+					echo_entry $file $len_s
+				else
+					echo "ERROR!!!" $file 1>&2
+				fi
+		    elif [[ -d "$file" ]]; then
+				directories+=($file)
+			fi
+		done
+	
+		for dir in $directories; do
+			search_dir $dir "$2/$dir"
+			dir_t=$(( dir_t + ? ))
+		done
+	fi
 
 	echo_entry "$2 total:" $dir_t
 	echo ""
